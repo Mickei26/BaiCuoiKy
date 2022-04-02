f9d5cde5f8aebba12311897d76275078553449a7
Mickey <dammit2525@yahoo.com.vn>
2022-04-02 10:11:00 +0700

    udpate

--- CMR_pgsqlAPI.php
+++ CMR_pgsqlAPI.php
@@ -297,7 +297,8 @@ function getInfoCMRToAjax($paPDO, $paSRID, $paPoint)
     $paPoint = str_replace(',', ' ', $paPoint);
     //$mySQLStr = "SELECT ST_AsGeoJson(geom) as geo from \"CMR_adm1\" where ST_Within('SRID=4326;POINT(12 5)'::geometry,geom)";
     //$mySQLStr = "SELECT ST_AsGeoJson(geom) as geo from \"CMR_adm1\" where ST_Within('SRID=".$paSRID.";".$paPoint."'::geometry,geom)";
-    $mySQLStr = "SELECT ten, kieu_bt, cap_bt, nam_dexuat, ST_Area(ST_Transform(geom, 26986))/1000000 as dientich from \"khu_bao_ton\" where ST_Within('SRID=" . $paSRID . ";" . $paPoint . "'::geometry,geom)";
+    //26918 là công thức
+    $mySQLStr = "SELECT ten, kieu_bt, cap_bt, nam_dexuat, ST_Area(ST_Transform(geom, 26918))/1000000 as dientich from \"khu_bao_ton\" where ST_Within('SRID=" . $paSRID . ";" . $paPoint . "'::geometry,geom)";
     $result = query($paPDO, $mySQLStr);
 
     if ($result != null) {
